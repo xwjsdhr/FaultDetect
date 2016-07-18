ced1a361d708a20157d630a9f8dcfb18b4bd9ca3
930324291@qq.com <930324291@qq.com>
2016-07-18 15:34:44 +0800

    test

--- app/src/main/java/info/faultdetect/com/faultdetect/bean/Req_Login.java
+++ app/src/main/java/info/faultdetect/com/faultdetect/bean/Req_Login.java
@@ -6,4 +6,21 @@ package info.faultdetect.com.faultdetect.bean;
 public class Req_Login {
     private String userInput;
     private String pwd;
+
+
+    public String getUserInput() {
+        return userInput;
+    }
+
+    public void setUserInput(String userInput) {
+        this.userInput = userInput;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
 }
